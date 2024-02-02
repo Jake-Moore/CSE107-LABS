6a366fe820ffd25d4a1c31dadb8a0786b646c85d
Jake Moore <jakealexandermoore@icloud.com>
2024-02-01 21:43:11 -0800

    remove .idea

---
+++ src/main/java/edu/jaalmoor/cse107/Lab2.java
@@ -0,0 +1,12 @@
+package edu.jaalmoor.cse107;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class Lab2 {
+    private static final DecimalFormat DF_3_DIGITS = new DecimalFormat("0.000");
+    // use one instance so we can expect uniformity of results
+    private static final Random RANDOM = new Random();
+
+
+}
